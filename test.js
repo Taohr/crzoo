ef6de564af50bec5262ba3798205c13b7cd1ca08
taohr <22227124@qq.com>
2020-01-10 18:10:44 +0800

    'some.fn'

--- test.js
+++ test.js
@@ -53,25 +53,43 @@ function createGrid(w, h) {
 
 grid = [
   [2,3,2,2,3],
-  [3,1,1,3,1],
-  [2,2,3,1,2],
+  [3,1,1,1,2],
+  [2,2,1,1,2],
   [2,3,2,1,3],
   [3,2,3,2,2],
 ]
-grid = grid.map((row)=>{
-  return row.map((it)=>{
-    return {val:it, st:Status.ready}
+grid = grid.map((row,y)=>{
+  return row.map((it,x)=>{
+    return {val:it, st:Status.ready, pos:{x:x,y:y}}
   })
 })
+// 行列格式输出矩阵
+function tlog(t) {
+  var s = t.map((row)=>{
+    return row.map((it)=>{
+      return JSON.stringify(it)
+    }).join(' ')
+  }).join('\n')
+  console.log(s)
+}
+function log() {
+  var arr = []
+  for (var i in arguments) {
+    var it = JSON.stringify(arguments[i])
+    arr.push(it)
+  }
+  console.log(arr.join(' '))
+  console.log()// print a \n
+}
 
 // 获取坐标(x,y)的格子
-function g(x,y) {
+function get(x,y) {
   var tmp = grid[y][x]
   return tmp
 }
 
 // 获取起始坐标(x,y)，宽高(w,h)区域的格子
-function g(x,y,w,h) {
+function sub(x,y,w,h) {
   var tmp = []
   var rows = grid.slice(y,y+h)
   for (i in rows) {
@@ -116,9 +134,69 @@ function rotate(grd,dir=1) {
   return tmp
 }
 
-var sub = g(1,1,3,3)
+// 二元化矩阵，元素等于val的为1，其他为0
+function bin(grd, val) {
+  return grd.map((r)=>{
+    return r.map((i)=>{
+      i.val = (i.val == val) ? 1 : 0
+      return i
+    })
+  })
+}
 
-console.log(rotate(Model.mL,0))
-console.log(rotate(Model.mL,1))
-console.log(rotate(Model.mL,2))
-console.log(rotate(Model.mL,3))
+// 以b为模板比较，对照b为1的位置，
+// 若a也为1则返回对应的索引值
+function compare(a, b) {
+  if (a.length != b.length || a[0].length != b[0].length) {
+    return null
+  }
+  var index = []
+  for (var r in b) {
+    for (var c in b[r]) {
+      if (!b[r][c]) {
+        continue
+      }
+      if (a[r][c].val) {
+        index.push(a[r][c].pos)
+      } else {
+        return null
+      }
+    }
+  }
+  return index.length == 0 ? null : index
+}
+
+// 某个模板要产生全部的变种
+// 应当通过怎样的旋转
+function rots(m) {
+  return m.length > 1 ? [0,1,2,3] : [0,1]
+}
+
+// 某个模板的尺寸
+// 对应该模板的优先级
+// 越大越高
+function msize(m) {
+  return m.length * m[0].length
+}
+
+var subb = sub(1,1,3,3)
+// tlog(subb)
+var binn = bin(subb,1)
+// tlog(binn)
+
+var rots = rots(Model.mL)
+for (var i in rots) {
+  m = rotate(Model.mL, rots[i])
+  var mch = compare(binn, m)
+  if (!mch) {
+    continue
+  }
+  for (var i in mch) {
+    var idx = mch[i]
+    var item = get(idx.x, idx.y)
+    item.st = Status.matched
+    log(item)
+  }
+}
+
+
