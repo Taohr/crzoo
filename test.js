6db86443a0ec4b3144607ac7a28b7b123e4ef06c
taohr <22227124@qq.com>
2020-01-13 12:18:27 +0800

    'match,all,models'

--- test.js
+++ test.js
@@ -1,17 +1,31 @@
 var grid = []
 const Status = {
-  ready:    0,//预备，静止状态，也可以是消除完毕后
-  toCheck:  1,//待查，标记的是发生变化的格子
-  checking: 2,//检查，所有要检查的，看是否同色的格子，标记已经被检查的
-  checked:  3,//查完，标记查完但不是要消除的格子
-  matched:  4,//匹配，标记可以被消除的格子
-  acting:   5,//消除，标记正在消除的格子，涉及延时动效的时候有用
+  empty:    0,//空白，等待填充新的方块
+  filling:  1,//填充，初始化或消除后会填充
+  ready:    2,//预备，静止状态，也可以是消除完毕后
+  toCheck:  3,//待查，标记的是发生变化的格子
+  checking: 4,//检查，所有要检查的，看是否同色的格子，标记已经被检查的
+  checked:  5,//查完，标记查完但不是要消除的格子
+  matched:  6,//匹配，标记可以被消除的格子
+  removing: 7,//消除，标记正在消除的格子，涉及延时动效的时候有用
 }
 
 const Model = {
   m3:[[1,1,1]],
   m4:[[1,1,1,1]],
   m5:[[1,1,1,1,1]],
+  m22:[
+    [1,1],
+    [1,1]
+  ],
+  m221:[
+    [1,1,0],
+    [1,1,1]
+  ],
+  m222:[
+    [1,1,1],
+    [1,1,0]
+  ],
   mL:[
     [1,0,0],
     [1,0,0],
@@ -53,9 +67,9 @@ function createGrid(w, h) {
 
 grid = [
   [2,3,2,2,3],
-  [3,1,1,1,2],
-  [2,2,1,1,2],
-  [2,3,2,1,3],
+  [3,1,1,2,2],
+  [2,1,1,2,2],
+  [2,3,1,1,3],
   [3,2,3,2,2],
 ]
 grid = grid.map((row,y)=>{
@@ -75,11 +89,11 @@ function tlog(t) {
 function log() {
   var arr = []
   for (var i in arguments) {
-    var it = JSON.stringify(arguments[i])
+    var it = typeof arguments[i] == 'object' ? JSON.stringify(arguments[i]) : arguments[i]
     arr.push(it)
   }
   console.log(arr.join(' '))
-  console.log()// print a \n
+  // console.log()// print a \n
 }
 
 // 获取坐标(x,y)的格子
@@ -169,6 +183,9 @@ function compare(a, b) {
 // 某个模板要产生全部的变种
 // 应当通过怎样的旋转
 function rots(m) {
+  if (m == Model.m22) {
+    return []
+  }
   return m.length > 1 ? [0,1,2,3] : [0,1]
 }
 
@@ -179,24 +196,51 @@ function msize(m) {
   return m.length * m[0].length
 }
 
-var subb = sub(1,1,3,3)
-// tlog(subb)
-var binn = bin(subb,1)
-// tlog(binn)
-
-var rots = rots(Model.mL)
-for (var i in rots) {
-  m = rotate(Model.mL, rots[i])
-  var mch = compare(binn, m)
-  if (!mch) {
-    continue
-  }
-  for (var i in mch) {
-    var idx = mch[i]
-    var item = get(idx.x, idx.y)
-    item.st = Status.matched
-    log(item)
-  }
+function offsets(grd, mdl) {
+  var v = grd.length - mdl.length +1
+  var h = grd[0].length - mdl[0].length + 1
+  v = Math.max(v, 0)
+  h = Math.max(h, 0)
+  return {h: h, v: v}
 }
 
+var models = []
+for (var k in Model) {
+  var model = Model[k]
+  var rs = rots(model)
+  for (var r in rs) {
+    m = rotate(model, rs[r])
+    models.push(m)
+  }
+}
+models.sort((a,b)=>{return msize(b)-msize(a)})
+
+for (var i in models) {
+  var model = models[i]
+  var offset = offsets(grid, model)
+  if (offset.h == 0 || offset.v == 0) {
+    // too big
+    continue
+  }
+  var h = model.length
+  var w = model[0].length
+  for (var y=0; y<offset.v; y++) {
+    for (var x=0; x<offset.h; x++) {
+      // 逐个匹配看是否满足消除的形状
+      var subb = sub(x,y,w,h)
+      var binn = bin(subb,1)
+      var mch = compare(binn, model)
+      if (!mch) {
+        continue
+      }
+      for (var i in mch) {
+        var idx = mch[i]
+        var item = get(idx.x, idx.y)
+        item.st = Status.matched
+        log(item)
+      }
+      log('---')
+    }
+  }
+}
 
