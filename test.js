5db23af760053c5acf259f22f33938f9db3fda46
taohr <22227124@qq.com>
2020-01-13 16:35:32 +0800

    'game-0.1'

--- test.js
+++ test.js
@@ -65,12 +65,16 @@ function createGrid(w, h) {
   return tmp
 }
 
+// 1 2 3 
 grid = [
-  [2,3,2,2,3],
-  [3,1,1,2,2],
-  [2,1,1,2,2],
-  [2,3,1,1,3],
-  [3,2,3,2,2],
+  [2,1,1,2,3,2,3],
+  [3,1,1,2,3,3,3],
+  [2,1,3,3,2,2,3],
+  [1,2,2,2,3,3,3],
+  [2,1,2,2,3,3,2],
+  [2,1,3,3,2,2,3],
+  [1,2,2,2,3,3,3],
+  [2,1,2,2,3,3,2],
 ]
 grid = grid.map((row,y)=>{
   return row.map((it,x)=>{
@@ -81,10 +85,11 @@ grid = grid.map((row,y)=>{
 function tlog(t) {
   var s = t.map((row)=>{
     return row.map((it)=>{
-      return JSON.stringify(it)
+      return typeof it == 'object' ? JSON.stringify(it) : it
     }).join(' ')
   }).join('\n')
   console.log(s)
+  console.log()
 }
 function log() {
   var arr = []
@@ -160,7 +165,7 @@ function bin(grd, val) {
 
 // 以b为模板比较，对照b为1的位置，
 // 若a也为1则返回对应的索引值
-function compare(a, b) {
+function compare(a, b, val) {
   if (a.length != b.length || a[0].length != b[0].length) {
     return null
   }
@@ -170,7 +175,7 @@ function compare(a, b) {
       if (!b[r][c]) {
         continue
       }
-      if (a[r][c].val) {
+      if (a[r][c].val == val && a[r][c].st != Status.matched) {
         index.push(a[r][c].pos)
       } else {
         return null
@@ -184,13 +189,13 @@ function compare(a, b) {
 // 应当通过怎样的旋转
 function rots(m) {
   if (m == Model.m22) {
-    return []
+    return [0]
   }
   return m.length > 1 ? [0,1,2,3] : [0,1]
 }
 
 // 某个模板的尺寸
-// 对应该模板的优先级
+// 对应模板的优先级
 // 越大越高
 function msize(m) {
   return m.length * m[0].length
@@ -204,6 +209,37 @@ function offsets(grd, mdl) {
   return {h: h, v: v}
 }
 
+// // 判断网格里是否有匹配的
+// // 如果有，这部分不再检查
+// function hasmatched(grd, val) {
+//   for (var r in grd) {
+//     for (var c in grd[r]) {
+//       var item = grd[r][c]
+//       if (item == val && item.st == Status.matched) {
+//         return true
+//       }
+//     }
+//   }
+//   return false
+// }
+
+// 排重，统计出现的值，对应出现的方块种类
+function distinct(grd) {
+  var dic = {}
+  for (var r in grd) {
+    for (var c in grd[r]) {
+      var item = grd[r][c]
+      dic[item.val] = 1
+    }
+  }
+  var keys = []
+  for (var k in dic) {
+    keys.push(+k)
+  }
+  return keys
+}
+
+// 准备所有的模板，包括旋转的变种
 var models = []
 for (var k in Model) {
   var model = Model[k]
@@ -213,34 +249,75 @@ for (var k in Model) {
     models.push(m)
   }
 }
+// 大模板优先
 models.sort((a,b)=>{return msize(b)-msize(a)})
 
-for (var i in models) {
-  var model = models[i]
-  var offset = offsets(grid, model)
-  if (offset.h == 0 || offset.v == 0) {
-    // too big
-    continue
-  }
-  var h = model.length
-  var w = model[0].length
-  for (var y=0; y<offset.v; y++) {
-    for (var x=0; x<offset.h; x++) {
-      // 逐个匹配看是否满足消除的形状
-      var subb = sub(x,y,w,h)
-      var binn = bin(subb,1)
-      var mch = compare(binn, model)
-      if (!mch) {
-        continue
+// 初始化后，全部检测。此时不是玩家操作触发
+function checkall(val) {
+  var matched = []
+  // 用模板匹配所有方块
+  for (var i in models) {
+    var model = models[i]
+    // 计算匹配全部范围，所需的偏移量
+    var offset = offsets(grid, model)
+    if (offset.h == 0 || offset.v == 0) {
+      continue
+    }
+    var h = model.length
+    var w = model[0].length
+    var onematch = []
+    for (var y=0; y<offset.v; y++) {
+      for (var x=0; x<offset.h; x++) {
+        // 逐个匹配看是否满足消除的形状
+        var subb = sub(x,y,w,h)
+        // if (hasmatched(subb, val)) {
+        //   continue
+        // }
+        var mch = compare(subb, model, val)
+        if (!mch) {
+          continue
+        }
+        for (var i in mch) {
+          var idx = mch[i]
+          var item = get(idx.x, idx.y)
+          onematch.push(item)
+          item.st = Status.matched
+        }
       }
-      for (var i in mch) {
-        var idx = mch[i]
-        var item = get(idx.x, idx.y)
-        item.st = Status.matched
-        log(item)
-      }
-      log('---')
+    }
+    if (onematch.length > 0) {
+      matched.push(onematch)
     }
   }
+  return matched
+}
+
+function logmatch(match, val) {
+  var w = grid[0].length
+  var h = grid.length
+  var arr = []
+  for (var r=0; r<h; r++) {
+    arr.push([])
+    for (var c=0; c<w; c++) {
+      arr[r].push('·')
+    }
+  }
+  for (var i in match) {
+    for (var j in match[i]) {
+      var item = match[i][j]
+      if (item.val == val) {
+        arr[item.pos.y][item.pos.x] = val
+      }
+    }
+  }
+  tlog(arr)
+}
+
+var allval = distinct(grid)
+log(allval)
+for (var i in allval) {
+  var val = allval[i]
+  var matched = checkall(val)
+  logmatch(matched, val)
 }
 
